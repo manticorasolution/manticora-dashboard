869bc5136124a3c07938e4eeca8c09fe9bbb4e12
pedromihael <pedromvpaes@gmail.com>
2020-10-10 17:36:57 -0400

    chore: change Header layout

--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 	display: grid;
-	grid-template-columns: 1fr 1.2fr;
-	grid-template-rows: 12vh 36vh 55vh;
+	grid-template-columns: 1fr 0.8fr;
+	grid-template-rows: 12vh 44vh 55vh;
 	grid-gap: 20px;
-	height: calc(100% - 8vh);
+	height: 100vh;
 	width: 100%;
 	margin: auto;
 	max-width: 95vw;
@@ -30,12 +30,12 @@ const Container = styled.div`
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		grid-template-rows: 1fr 1fr;
-		grid-gap: 10px;
+		grid-gap: 15px;
 		height: 100%;
 
 		.card1, .card2, .card3, .card4 { 
 			background: white;
-			border-radius: 10px;
+			border-radius: 5px;
 		}
 
 	}
@@ -43,13 +43,13 @@ const Container = styled.div`
 	.cards-right-up {
 		background: white;
 		grid-area: cardsRightUp;
-		border-radius: 10px;
+		border-radius: 5px;
 	}
 
 	.cards-left-down {
 		background: white;
 		grid-area: cardsLeftDown;
-		border-radius: 10px;
+		border-radius: 5px;
 	}
 
 	.cards-right-down {
@@ -71,14 +71,14 @@ const Container = styled.div`
 
 			.alert1, .alert2 {
 				background: salmon;
-				border-radius: 10px;
+				border-radius: 5px;
 			}
 		}
 
 		.in-alert-products {
 			background: white;
 			grid-area: inAlertProducts;
-			border-radius: 10px;
+			border-radius: 5px;
 		}
 	}
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Container, HeaderOptions, HeaderLogo } from './styles';
-import Button from '../Button';
+import { Container, HeaderButtons, HeaderProfile, HeaderLogo } from './styles';
 import logo from '../../imgs/logo.png'
+import { FaBell } from 'react-icons/fa';
 
 const Header = () => {
 	return <Container>
 		<HeaderLogo>
 			<img src={logo} alt="Manticora logo" />
-			<h2 contentEditable>Mercadinho da Dona Maria</h2>
 		</HeaderLogo>
-		<HeaderOptions>
-			<li>Relatórios Detalhados</li>
-			<b><li>Editar Perfil</li></b>
-			<li><Button text="Ver Estoque" /></li>
-		</HeaderOptions>
+		<HeaderButtons>
+			<ul>
+				<b><li key="1" id="general">Geral</li></b>
+				<li key="2" id="growth">Escala de Crescimento</li>
+				<li key="3" id="stock">Previsão de Estoque</li>
+				<li key="4" id="expire">Validade dos Produtos</li>
+			</ul>
+		</HeaderButtons>
+		<HeaderProfile>
+			<FaBell color="#dadada" size="3vh" />
+			<div className="profile-icon"></div>
+		</HeaderProfile>
 	</Container>
 }
 
