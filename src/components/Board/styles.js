acdadf7a7ae4e39c4271a24fc0b31338d088fba9
pedromihael <pedromvpaes@gmail.com>
2020-10-10 16:43:22 -0400

    chore: add border radius

--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -33,21 +33,23 @@ const Container = styled.div`
 		grid-gap: 10px;
 		height: 100%;
 
-		.card1 { background: yellow }
-		.card2 { background: yellow }
-		.card3 { background: yellow }
-		.card4 { background: yellow }
+		.card1, .card2, .card3, .card4 { 
+			background: white;
+			border-radius: 10px;
+		}
 
 	}
 
 	.cards-right-up {
-		background: blue;
+		background: white;
 		grid-area: cardsRightUp;
+		border-radius: 10px;
 	}
 
 	.cards-left-down {
-		background: red;
+		background: white;
 		grid-area: cardsLeftDown;
+		border-radius: 10px;
 	}
 
 	.cards-right-down {
@@ -67,13 +69,16 @@ const Container = styled.div`
 			grid-row-gap: 10px;
 			grid-area: alerts;
 
-			.alert1 { background: salmon }
-			.alert2 { background: salmon }
+			.alert1, .alert2 {
+				background: salmon;
+				border-radius: 10px;
+			}
 		}
 
 		.in-alert-products {
-			background: green;
-			grid-area: inAlertProducts
+			background: white;
+			grid-area: inAlertProducts;
+			border-radius: 10px;
 		}
 	}
 `;
