e8c5d6043962cfa405fd6834128a56d5c4826749
pedromihael <pedromvpaes@gmail.com>
2020-10-11 10:58:30 -0400

    chore: change line chart colors

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,6 +3,7 @@ import Container from './styles';
 import InAlertProductsTable from '../Table/inAlertProductsTable';
 import BestSellersTable from '../Table/bestSellersTable';
 import LineChart from '../Chart/lineChart';
+import BarChart from '../Chart/barChart';
 
 const Card = ({
 	className,
@@ -14,6 +15,7 @@ const Card = ({
 	hasInAlertTable,
 	hasBestSellersTable,
 	hasLineChart,
+	hasBarChart,
 }) => {
 	return (
 		<Container className={className}>
@@ -30,6 +32,7 @@ const Card = ({
 			{hasInAlertTable && <InAlertProductsTable />}
 			{hasBestSellersTable && <BestSellersTable />}
 			{hasLineChart && <LineChart />}
+			{hasBarChart && <BarChart />}
 			<p className="revenues">{revenues}</p>
 			<p className="consideration">{consideration}</p>
 		</Container>
